0aa061ad9af8f2cc4291c3d20c3ce58601ae4638
1234Es <esenurer@gmail.com>
2023-04-05 18:00:16 +0100

    Styled page a little bit focusing on navbar and first few sections

---
+++ myscripts.js
@@ -0,0 +1,8 @@
+function navmenu() {
+    var x = document.getElementById("navitem");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
